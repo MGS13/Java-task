e73826753ca7ecf28df1e91180e7c25a623db69d
aasish23 <47400245+aasish23@users.noreply.github.com>
2022-03-16 17:32:48 -0400

    add: added repo and model files

---
+++ src/main/java/com/todo/todo/model/Options.java
@@ -0,0 +1,29 @@
+package com.todo.todo.model;
+
+public class Options {
+    private Boolean task1, task2, task3;
+
+    public void setTask1(Boolean task1) {
+        this.task1 = task1;
+    }
+
+    public void setTask2(Boolean task2) {
+        this.task2 = task2;
+    }
+
+    public void setTask3(Boolean task3) {
+        this.task3 = task3;
+    }
+
+    public Boolean getTask1() {
+        return this.task1;
+    }
+
+    public Boolean getTask2() {
+        return this.task2;
+    }
+
+    public Boolean getTask3() {
+        return this.task3;
+    }
+}
---
+++ src/main/java/com/todo/todo/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.todo.todo.repository;
+
+import com.todo.todo.model.Options;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Options, String> {
+    
+}
